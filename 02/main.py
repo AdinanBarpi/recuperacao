5c695fdd00a9dbbf4b462bc8c42ffc21ae58ef8a
AdinanBarpi <89356846+AdinanBarpi@users.noreply.github.com>
2021-09-03 01:02:13 -0300

    Recuperação

---
+++ 02/main.py
@@ -0,0 +1,6 @@
+from validador import Validador
+
+vetor_valores = [999,0.3,0.7]
+total = Validador.soma(vetor_valores)
+
+print("Resultado da soma de todos os valores do vetor = ",str(total))
---
+++ 02/validador.py
@@ -0,0 +1,9 @@
+class Validador:
+
+    @staticmethod
+
+    def soma(vetor_valores):
+        total = 0
+        for valor in vetor_valores:
+            total = total + valor
+        return total
